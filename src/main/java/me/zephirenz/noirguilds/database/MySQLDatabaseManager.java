d19cc7599bcb5445c33dbbb9d5d77463278e7069
Brynley McDonald <brynleymcdonald@gmail.com>
2013-09-18 08:14:31 +1200

    Add unimplemented methods to MySQL DB

--- src/main/java/me/zephirenz/noirguilds/database/MySQLDatabaseManager.java
+++ src/main/java/me/zephirenz/noirguilds/database/MySQLDatabaseManager.java
@@ -1,4 +1,31 @@
 package me.zephirenz.noirguilds.database;
 
+import me.zephirenz.noirguilds.objects.Guild;
+import me.zephirenz.noirguilds.objects.GuildPlayer;
+import me.zephirenz.noirguilds.objects.GuildRank;
+
 public class MySQLDatabaseManager implements DatabaseManager {
+    public Guild getGuildByName(String guildName) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Guild getGuildByOwner(String player) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Guild getGuildByPlayer(String player) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public GuildPlayer getGPlayerByGuild(String guild) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public GuildRank getPlayerRank(String player) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Guild[] getGuilds() {
+        return new Guild[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
