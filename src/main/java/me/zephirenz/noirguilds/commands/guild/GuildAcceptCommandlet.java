374898d111e97a0a01e483ea82e1321d6f418bb4
ZephireNZ <brynleymcdonald@gmail.com>
2013-10-24 19:37:29 +1300

    Stop people from inviting multiple people to get past limit

--- src/main/java/me/zephirenz/noirguilds/commands/guild/GuildAcceptCommandlet.java
+++ src/main/java/me/zephirenz/noirguilds/commands/guild/GuildAcceptCommandlet.java
@@ -2,6 +2,7 @@ package me.zephirenz.noirguilds.commands.guild;
 
 import me.zephirenz.noirguilds.GuildsHandler;
 import me.zephirenz.noirguilds.NoirGuilds;
+import me.zephirenz.noirguilds.config.PluginConfig;
 import me.zephirenz.noirguilds.objects.Guild;
 import me.zephirenz.noirguilds.objects.GuildMember;
 import me.zephirenz.noirguilds.objects.GuildRank;
@@ -15,10 +16,12 @@ public class GuildAcceptCommandlet {
 
     private final NoirGuilds plugin;
     private final GuildsHandler gHandler;
+    private final PluginConfig pConfig;
 
     public GuildAcceptCommandlet() {
         this.plugin = NoirGuilds.inst();
         this.gHandler = plugin.getGuildsHandler();
+        this.pConfig = PluginConfig.getInstance();
     }
 
     /**
@@ -51,6 +54,11 @@ public class GuildAcceptCommandlet {
 
         inviteTask.cancel();
         gHandler.removeInvite(inviteTask);
+
+        if(data.getGuild().getMembers().size() >= pConfig.getMemberLimit() && pConfig.getMemberLimit() > 0) {
+            plugin.sendMessage(sender, "The guild has the maximum number of members.");
+            return;
+        }
 
         Guild guild = data.getGuild();
         GuildRank rank = guild.getDefaultRank();
